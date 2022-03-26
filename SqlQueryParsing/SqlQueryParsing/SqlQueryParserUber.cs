d429bd9e0e1178cde2eda2f08d9206a239b7da70
Roe Iton <itonro@amazon.com>
2022-03-26 12:45:47 +0300

    Add SqlQueryParsing file

---
+++ SqlQueryParsing/SqlQueryParsing/SqlQueryParserUber.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace SqlQueryParsing
+{
+    public class SqlQueryParserUber
+    {
+        
+
+        public static string ParseStringToQuery(string s)
+        {
+            string query = "result";
+            return query;
+        }
+
+        public static string SendRequestToUberParser(string s)
+        {
+            // mashu ParseStringToQuery(s)
+        }
+
+        public static QueryParsingResult GetResultParser(string s)
+        {
+            string resultFromUber = SendRequestToUberParser(s)
+
+
+        }
+
+        public SqlQueryParserUber()
+        {
+
+        }
+    }
+}
+
+
+
+
+// input: String ->
+// """"""" ' 
+
+/*
+ * 
+ * curl -X POST http://52.214.88.62:3000/parser \
+                                             -H 'Content-Type: application/json' \
+                                             -d '{"query": "${query}"}'
+ */
